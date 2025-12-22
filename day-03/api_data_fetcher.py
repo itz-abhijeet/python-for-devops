ca2115ebd136d6de80ad6fb1e6c1701ddf1946d0
itz-abhijeet <aabhipawar26@gmail.com>
2025-12-22 15:49:00 +0530

    Task: Day-03 Add exceptions

--- day-03/api_data_fetcher.py
+++ day-03/api_data_fetcher.py
@@ -3,41 +3,49 @@ import json
 
 def get_usd_shareholders_data(url, headers):
     try:
-        response = requests.get(url=url, headers=headers)
-        response.raise_for_status
+        response = requests.get(url=url, headers=headers, timeout=10)
+        
+        response.raise_for_status() 
+        
         data_json = response.json()
         filtered_results = []
 
-        if "results" in data_json:
+        if "results" in data_json and isinstance(data_json["results"], list):
             for item in data_json["results"]:
                 records = {
-                    "id" : item.get('id', '0'),
-                    "ticker" : item.get('ticker', 'N/A'),
-                    "cash_amount" : item.get('cash_amount', '0'),
-                    "record_date" : item.get('record_date', 'N/A')
+                    "id": item.get('id', '0'),
+                    "ticker": item.get('ticker', 'N/A'),
+                    "cash_amount": item.get('cash_amount', '0'),
+                    "record_date": item.get('record_date', 'N/A')
                 }
 
-                print(f"ID: {records["id"]} | Ticker: {records["ticker"]} | Amount: {records["cash_amount"]} | Record Date: {records["cash_amount"]}")
+                print(f"ID: {records['id']} | Ticker: {records['ticker']} | "
+                      f"Amount: {records['cash_amount']} | Record Date: {records['record_date']}")
 
                 filtered_results.append(records)
         else:
-            print("No results found")
+            print("No results found in the response.")
 
-        with open("output.json","w") as f:
+        with open("output.json", "w") as f:
             json.dump(filtered_results, f, indent=4)
+        print("\nSuccessfully saved data to output.json")
 
-    except PermissionError as perm_err:
-        print(f"Error: {perm_err}")
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.ConnectionError:
+        print("Error: Could not connect to the server. Check your internet.")
+    except json.JSONDecodeError:
+        print("Error: Failed to parse API response as JSON.")
     except Exception as err:
-        print(f"Unexpected error occured: {err}")
-        
+        print(f"An unexpected error occurred: {err}")
 
 if __name__ == "__main__":
-
-    api_key = "API_KEY" 
-    base_url= "https://api.massive.com/v3/reference/dividends"
-    headers = {
+    api_key = "gzRn7DddPG5GUpYiGBZuArfpIO8IFJLH" 
+    base_url = "https://api.massive.com/v3/reference/dividends"
+    
+    request_headers = {
         "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
     }
 
-    get_usd_shareholders_data(base_url, headers)
+    get_usd_shareholders_data(base_url, request_headers)
