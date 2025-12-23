86431166deec75f36938975f74201a2da721d702
itz-abhijeet <aabhipawar26@gmail.com>
2025-12-23 18:42:12 +0530

    Task: Day-04 handled empty files, inserted data into dict

--- day-04/log_analyzer.py
+++ day-04/log_analyzer.py
@@ -5,6 +5,9 @@ def get_logs(log_file_name):
     try:
         with open(f"{log_file_name}", "r")as log_file:
             logs = log_file.read()
+            if logs == "":
+                print(f"{log_file_name} is empty")
+                sys.exit()
             return logs
     except FileNotFoundError:
         print(f"File {log_file_name} Not found")
@@ -17,25 +20,24 @@ def summarize_logs(logs):
     info = logs.count("INFO")
     warning = logs.count("WARNING")
     error = logs.count("ERROR")
-    return info, warning, error
-
-def print_logs_summary(info, warning, error):
-    print(f"INFO : {info}\n"
-          f"WARNING : {warning}\n"
-          f"ERROR : {error}")
-
-def write_logs_summary(info, warning, error):
     log_summary = {
         "INFO" : f"{info}",
         "WARNING" : f"{warning}",
         "ERROR" : f"{error}"
     }
+    return log_summary
+
+def print_logs_summary(log_summary):
+    for status, count in log_summary.items():
+        print(f"{status} : {count}")
+
+def write_logs_summary(log_summary):
     with open("log_summary.json", "w")as log_summary_file:
         json.dump(log_summary, log_summary_file, indent=4)
 
 if __name__ == "__main__":
     log_file_name = input("Enter the name of log file with extension: ")
     logs = get_logs(log_file_name)
-    info, warning, error = summarize_logs(logs)
-    print_logs_summary(info, warning, error)
-    write_logs_summary(info, warning, error)
+    log_summary = summarize_logs(logs)
+    print_logs_summary(log_summary)
+    write_logs_summary(log_summary)
